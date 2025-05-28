ce136a8ca4d20b4f2702458a5088c5889bd1bffc
MichealRaj26 <hentry1001@gmail.com>
2025-05-28 12:21:40 +0530

    constants

--- php/05_define.php
+++ php/05_define.php
@@ -9,6 +9,23 @@
 
 <body>
 
+    <?php
+
+        // Defining constants
+        // syntax: define("CONSTANT_NAME", "value");
+        define("PROGRAM", "PHP");
+        define("VERSION", "8.2");
+
+        define("NAMES", ["John", "Joel", "Jero"]);
+
+        echo "<h1> Welcome to learn " . PROGRAM . " of Version " . VERSION . "</h1>";
+
+        echo "<h2>Name : " .NAMES[0] . "</h2>";
+
+        echo "<h2>Names: " .NAMES[1] . ", " . NAMES[2] . "</h2>";
+
+    ?>
+
 </body>
 
 </html>
