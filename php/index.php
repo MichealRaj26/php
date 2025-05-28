10b539c472efca57a44e4a48233ad104beb2bb53
MichealRaj26 <hentry1001@gmail.com>
2025-05-28 11:40:53 +0530

    j

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "PHP Learning Path - Basic PHP Script<br>";
+    echo "This is a basic PHP script that outputs text to the browser.<br>";
+
+    echo "<h1>You can just use the tags inside the echo's like this:</h1>";
+    echo "<p>This is a paragraph inside an echo statement.</p>";
+
+    
+    ?>
+</body>
+
+</html>
